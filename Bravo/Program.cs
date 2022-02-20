2a640b604a60f263974b5630de15b11310caa2f5
SwampMaster2160 <62127482+SwampMaster2160@users.noreply.github.com>
2022-02-21 12:37:55 +1300

    2

--- Bravo/Program.cs
+++ Bravo/Program.cs
@@ -22,33 +22,49 @@ namespace Bravo
     {
         static void Main(string[] args)
         {
+            // Init employee list
             List<Employee> employees = new List<Employee>();
+            // Print starting info
             Console.WriteLine("==========Employee Information==========");
-            Console.Write("Functions:\n1: Add employee.\n2: Insert employee.\n3: Update employee.\n4: Delete employee.\n5: Search employee.\n6: Display employee.\n");
-            while (true)
+            Console.Write("Functions:\n1: Add employee.\n2: Insert employee.\n3: Update employee.\n4: Delete employee.\n5: Search employee.\n6: Display employee.\n7: Exit.\n");
+            // Enter running loop
+            bool running = true;
+            while (running)
             {
+                // Get function number
                 Console.Write("Please enter function number: ");
                 char function = Console.ReadKey().KeyChar;
                 Console.WriteLine();
+                // Run the function
                 switch (function)
                 {
                     case '1':
+                    case 'a':
                         Add(employees);
                         break;
                     case '2':
+                    case 'i':
                         Insert(employees);
                         break;
                     case '3':
+                    case 'u':
                         Update(employees);
                         break;
                     case '4':
+                    case 'd':
                         Delete(employees);
                         break;
                     case '5':
+                    case 's':
                         Search(employees);
                         break;
                     case '6':
+                    case 'v':
                         Display(employees);
+                        break;
+                    case '7':
+                    case 'e':
+                        running = false;
                         break;
                     default:
                         Console.WriteLine("Invalid function.");
@@ -57,50 +73,107 @@ namespace Bravo
             }
         }
 
+        // Get a valid list index from the console.
+        // If isPickLocation is set then the function will get an index for picking/replaceing/deleting an employee, else it will get one for inserting at the location.
+        static int GetValidListIndex(List<Employee> employees, bool isPickLocation)
+        {
+            try
+            {
+                int count = employees.Count;
+                int returnVal = int.Parse(Console.ReadLine());
+                if (isPickLocation)
+                {
+                    if (count == 0)
+                    {
+                        Console.WriteLine("Error: employee list is empty.");
+                        return -1;
+                    }
+                    count -= 1;
+                }
+                if (returnVal < 0)
+                {
+                    Console.WriteLine("Error: index is negitive, it must be at least 0.");
+                    return -1;
+                }
+                if (count < returnVal)
+                {
+                    Console.WriteLine("Error: index is too large, must be at most " + count + ".");
+                    return -1;
+                }
+                return returnVal;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: invalid number entered.");
+                return -1;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: number entered is outside of valid range.");
+                return -1;
+            }
+        }
+
         static void Add(List<Employee> employees)
         {
+            // Get names
             Console.Write("Enter first name: ");
             string firstName = Console.ReadLine();
             Console.Write("Enter last name: ");
             string lastName = Console.ReadLine();
+            // Add to list
             employees.Add(new Employee(firstName, lastName));
         }
 
         static void Insert(List<Employee> employees)
         {
+            // Get location
             Console.Write("Enter the insertion location: ");
-            int insertLocation = int.Parse(Console.ReadLine());
+            int insertLocation = GetValidListIndex(employees, false);
+            if (insertLocation == -1) return;
+            // Get names
             Console.Write("Enter first name: ");
             string firstName = Console.ReadLine();
             Console.Write("Enter last name: ");
             string lastName = Console.ReadLine();
+            // Insert
             employees.Insert(insertLocation, new Employee(firstName, lastName));
         }
 
         static void Update(List<Employee> employees)
         {
+            // Get location
+            Console.Write("Enter the insertion location: ");
+            int location = GetValidListIndex(employees, true);
+            if (location == -1) return;
+            // Get name
             Console.Write("Enter first name: ");
             string firstName = Console.ReadLine();
             Console.Write("Enter last name: ");
             string lastName = Console.ReadLine();
-            Console.Write("Enter the insertion location: ");
-            int location = int.Parse(Console.ReadLine());
+            // Update
             employees[location] = new Employee(firstName, lastName);
         }
 
         static void Delete(List<Employee> employees)
         {
+            // Get location
             Console.Write("Enter the employee ID to delete: ");
-            int id = int.Parse(Console.ReadLine());
+            int id = GetValidListIndex(employees, true);
+            if (id == -1) return;
+            // Delete
             employees.RemoveAt(id);
         }
 
         static void Search(List<Employee> employees)
         {
+            // Get name
             Console.Write("Enter first name: ");
             string firstName = Console.ReadLine();
-            for (int x = 0; x < employees.Count(); x++)
+            // Loop thrugh all names
+            for (int x = 0; x < employees.Count; x++)
             {
+                // Print out a employee is their name is matches the search name
                 if (employees[x].FirstName == firstName)
                 {
                     Console.WriteLine(x + ": " + employees[x].FirstName + " " + employees[x].LastName + ".");
@@ -110,11 +183,21 @@ namespace Bravo
 
         static void Display(List<Employee> employees)
         {
-            Console.WriteLine("Employees:");
-            for (int x = 0; x < employees.Count(); x++)
+            // If the employee list is empty then say it is empty
+            if (employees.Count == 0)
             {
-                Employee employee = employees[x];
-                Console.WriteLine(x + ": " + employee.FirstName + " " + employee.LastName + ".");
+                Console.WriteLine("Employee list is empty.");
+                return;
+            }
+            // Else print out all employees
+            else
+            {
+                Console.WriteLine("Employees:");
+                for (int x = 0; x < employees.Count; x++)
+                {
+                    Employee employee = employees[x];
+                    Console.WriteLine(x + ": " + employee.FirstName + " " + employee.LastName + ".");
+                }
             }
         }
     }
