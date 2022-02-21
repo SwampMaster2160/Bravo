e9ef17e7098accf6bf3655c1ff63e88acf794935
SwampMaster2160 <62127482+SwampMaster2160@users.noreply.github.com>
2022-02-22 12:11:45 +1300

    4

--- Bravo/Program.cs
+++ Bravo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,7 +30,7 @@ namespace Bravo
         static void Main(string[] args)
         {
             // Init employee list
-            List<Employee> employees = new List<Employee>();
+            List<Employee> employees = LoadData();
             // Print starting info
             Console.WriteLine("==========Employee Information==========");
             Console.Write("Functions:\n1: Add employee.\n2: Insert employee.\n3: Update employee.\n4: Delete employee.\n5: Search employee.\n6: Display employee.\n7: Exit.\n");
@@ -77,6 +78,77 @@ namespace Bravo
                         break;
                 }
             }
+            SaveData(employees);
+        }
+
+        static void SaveData(List<Employee> employees)
+        {
+            // Open file
+            System.IO.StreamWriter file = File.CreateText(@"database.db");
+            foreach (Employee employee in employees)
+            {
+                // Write the names
+                file.WriteLine(employee.FirstName);
+                file.WriteLine(employee.LastName);
+                // Write the optional feilds if they are not blank
+                if (0 < employee.Email.Count()) file.WriteLine("-email\n" + employee.Email);
+                if (0 < employee.PhoneNumber.Count()) file.WriteLine("-phone\n" + employee.PhoneNumber);
+                if (0 < employee.Address.Count()) file.WriteLine("-address\n" + employee.Address);
+                // Write the person end symbol
+                file.WriteLine("---");
+            }
+            // Close file
+            file.Close();
+        }
+
+        static List<Employee> LoadData()
+        {
+            // Create a blank list if there is no file
+            if (!File.Exists(@"database.db")) return new List<Employee>();
+            // Otherwise open file
+            List<Employee> list = new List<Employee>();
+            System.IO.StreamReader file = File.OpenText(@"database.db");
+
+            // Init vars
+            string firstName = "";
+            string lastName = "";
+            string email = "";
+            string phoneNumber = "";
+            string address = "";
+            string varName = "";
+            uint lineOfPerson = 1;
+            string line;
+            // For each line in the file
+            while((line = file.ReadLine()) != null)
+            {
+                // If person end symbol then create person object
+                if (line == "---")
+                {
+                    list.Add(new Employee(firstName, lastName, email, phoneNumber, address));
+                    lineOfPerson = 0;
+                    firstName = "";
+                    lastName = "";
+                    email = "";
+                    phoneNumber = "";
+                    address = "";
+                    varName = "";
+                }
+                // Read data
+                if (lineOfPerson == 1) firstName = line;
+                else if (lineOfPerson == 2) lastName = line;
+                else if (lineOfPerson % 2 == 1) varName = line;
+                else if (lineOfPerson % 2 == 0)
+                {
+                    if (varName == "-email") email = line;
+                    if (varName == "-phone") phoneNumber = line;
+                    if (varName == "-address") address = line;
+                }
+                lineOfPerson++;
+            }
+
+            // Close file
+            file.Close();
+            return list;
         }
 
         // Get a valid list index from the console.
